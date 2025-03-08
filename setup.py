818d19863bf3f186f1db9ea1926797b815df773d
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2025-03-08 09:42:32 -0600

    ci: update gha and bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name="girder-virtual-resources",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="2.0.0",
+    version="2.0.1",
     description="Girder Plugin exposing physical folders and files as Girder objects.",
     packages=find_packages(),
     include_package_data=True,
